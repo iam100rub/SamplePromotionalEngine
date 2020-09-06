c36c056dacb4986335d3bb1045bd5dd8e57df21f
s.sauravgandhi@gmail.com <s.sauravgandhi@gmail.com>
2020-09-06 23:34:02 +0530

    Added Model for Promotion Engine

---
+++ PromotionEngine/Model/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Model
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public List<Product> Products { get; set; }
+        public Order(int orderId, List<Product> products)
+        {
+            this.OrderID = orderId;
+            this.Products = products;
+        }
+    }
+}
---
+++ PromotionEngine/Model/Promotion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine.Model
+{
+    public class Promotion
+    {
+        public int PromotionID { get; set; }
+        public Dictionary<string, int> ProductInfo { get; set; }
+        public decimal PromoPrice { get; set; }
+
+        public Promotion(int promotionId, Dictionary<string, int> productInfo, decimal promoPrice)
+        {
+            this.PromotionID = promotionId;
+            this.ProductInfo = productInfo;
+            this.PromoPrice = promoPrice;
+        }
+    }
+}
