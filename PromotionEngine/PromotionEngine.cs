43aaade912c75c29937c711a6360fd48696adde6
s.sauravgandhi@gmail.com <s.sauravgandhi@gmail.com>
2020-09-06 23:29:43 +0530

    Added Promotion Engine Project

---
+++ PromotionEngine/PromotionEngine.cs
@@ -0,0 +1,26 @@
+﻿using PromotionEngine.HelperClass;
+using PromotionEngine.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromotionEngine
+{
+    class PromotionEngine
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+    
+   
+    
+    
+   
+    
+}
